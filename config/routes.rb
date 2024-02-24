51cae57e68823fd9163fa1d0e1a42c22e42968b5
n-haruhi <hr23nakanishi@gmail.com>
2024-02-24 14:28:18 +0000

    [Update]bookモデルコントローラアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
+
+  resources :books, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
