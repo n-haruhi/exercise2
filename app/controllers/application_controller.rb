080146dc8be001681b45bd1faa1a974338d87817
n-haruhi <hr23nakanishi@gmail.com>
2024-02-25 11:58:13 +0000

    [Update]画面遷移設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_up_path_for(resource)
+    about_path
+  end
+
   # protectedは呼び出された他のコントローラからも参照することができる。
   protected
 
