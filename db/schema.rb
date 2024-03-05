be19912eb40d66a8068f334bd454f296821c39db
n-haruhi <hr23nakanishi@gmail.com>
2024-03-05 07:22:57 +0000

    [Update]ユーザーレイアウト調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_28_091854) do
+ActiveRecord::Schema.define(version: 2024_03_05_055750) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2024_02_28_091854) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "opinion"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
