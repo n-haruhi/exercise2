28aca78b5ed56d1643339620999dd96fe581d0f5
n-haruhi <hr23nakanishi@gmail.com>
2024-03-06 07:30:19 +0000

    [Finish]修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_05_055750) do
+ActiveRecord::Schema.define(version: 2024_03_05_073358) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2024_03_05_055750) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.text "opinion"
+    t.text "body"
     t.integer "user_id"
   end
 
